e2d643a96b0b0764ee03adf3b7f667a145f98205
Deven Jahnke <10586846+devenjahnke@users.noreply.github.com>
2025-05-28 11:34:52 -0400

    code style

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,7 +4,7 @@ namespace Aryeo\Kafkaesque\Tests;
 
 class ExampleTest extends TestCase
 {
-    function test_true_is_true()
+    public function test_true_is_true()
     {
         $this->assertTrue(true);
     }
